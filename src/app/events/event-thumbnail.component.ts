6cfd6b80c936da6d168170b25a392f3e47b42d7e
Jason <jaylima0@gmail.com>
2020-09-29 11:32:38 -0400

    Using built in pipes

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,7 +5,7 @@ import { IEvent } from "./shared/event.model";
     selector: 'event-thumbnail',
     template: `
         <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
-            <h2>{{event?.name}}</h2>
+            <h2>{{event?.name | uppercase}}</h2>
             <div>Date: {{event?.date}}</div>
             <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
                 Time: {{event?.time}}
@@ -13,7 +13,7 @@ import { IEvent } from "./shared/event.model";
                 <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
                 <span *ngSwitchDefault>(Normal Start)</span>
             </div>
-            <div>Price: \${{ event?.price }}</div>
+            <div>Price: {{ event?.price | currency:'USD' }}</div>
             <div *ngIf="event?.location">
                 <span>Location: {{event?.location?.address}}</span>
                 <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
