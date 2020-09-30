92abea53c2562c20dedb3f2a6004a16fc57158ef
Jason <jaylima0@gmail.com>
2020-09-29 21:13:46 -0400

    10:Filtering Data - Session filter not working. Comeback and do

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -20,6 +20,7 @@ import { IEvent, ISession } from "../shared/index";
 export class EventDetailsComponent {
     event: IEvent
     addMode: boolean
+    filterBy: string = 'all'
 
     constructor(private eventService: EventService, private route: ActivatedRoute) {
 
