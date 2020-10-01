6e74340b2bc8d2cf4041e130d96a71c32a03d2c5
Jason <jaylima0@gmail.com>
2020-10-01 10:59:51 -0400

    Binding an ID to use in modal directive and modal

--- src/app/common/modal-trigger.directive.ts
+++ src/app/common/modal-trigger.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Inject, OnInit, ElementRef } from "@angular/core";
+import { Directive, Inject, OnInit, ElementRef, Input } from "@angular/core";
 import { JQ_TOKEN } from "./jQuery.service";
 
 @Directive({
@@ -6,14 +6,14 @@ import { JQ_TOKEN } from "./jQuery.service";
 })
 export class ModalTriggerDirective implements OnInit {
     private el: HTMLElement
-
+    @Input('modal-trigger') modalId : string
     constructor(ref: ElementRef, @Inject(JQ_TOKEN) private $ : any) {
         this.el = ref.nativeElement
     }
 
     ngOnInit() {
         this.el.addEventListener('click', e => {
-            this.$('#simple-modal').modal({})
+            this.$(`#${this.modalId}`).modal({})
         })
     }
 }
